967477d831fadcc96b073a0167a2efe2236cffd5
afeilulu <afeilulu@gmail.com>
2026-01-29 11:49:50 +0800

    update ps1

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -10,6 +10,7 @@ Import-Module -Name Terminal-Icons
 
 # Ensure console's output encoding is set to UTF-8
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$env:LESSCHARSET = 'utf-8'
 
 # POSH_THEMES_PATH: C:\Users\ChenGefei\AppData\Local\Programs\oh-my-posh\themes
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH\gruvbox.omp.json | Invoke-Expression
@@ -19,3 +20,4 @@ Set-Alias -Name vi -Value E:\App\nvim-win64\bin\nvim.exe
 
 # Claude Code
 $env:ENABLE_LSP_TOOLS = "1"
+$env:CLAUDE_CODE_EDITOR="code"
